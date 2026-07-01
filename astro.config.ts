dcde854bc492562264e245f081afae4eb652ac45
mmaurello <marioj@opslayer.com>
2026-07-01 21:00:05 +0200

    add custom domain

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://mmaurello.github.io",
+  site: "https://mjmaurello.dev",
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
